7bc96325a077894c07fcd39d804bc1659ac5d7d2
Mohamed El Mahdi Trabelsi <trabelsimahdi79@gmail.com>
2023-10-02 13:26:19 +0100

    Header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Header} from './components/index';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element = {<h1>hello</h1>}/>
+        <Route path="/" element = {<Header/> }/>
           
       </Routes>
     </BrowserRouter>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,35 @@
 import './header.scss';
+import {logo} from '../../assets/index';
+import { HeartOutlined , SearchOutlined , ShoppingCartOutlined , UserOutlined } from '@ant-design/icons';
 const Header = () => {
   return (
     <div className='header'>
-        
+        <div className='header__left'>
+            <img src={logo} alt='logo' />
+        </div>
+        <div className='header__middle'>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>Blog</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        <div className='header__left'>
+            <div className="header__left__search">
+                <SearchOutlined/>
+            </div>
+            <div className="header__left__authentication">
+                <UserOutlined/>
+            </div>
+            <div className="header__left__favorits">
+                <HeartOutlined/>
+            </div>
+            <div className="header__left__cart">
+                <ShoppingCartOutlined/>
+            </div>
+        </div>  
     </div>
   )
 }
