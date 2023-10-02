5d03b30371b6702edde6762b5e2609b9127fc7e9
Mohamed El Mahdi Trabelsi <trabelsimahdi79@gmail.com>
2023-10-02 18:49:20 +0100

    working on header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,37 +1,69 @@
 import './header.scss';
-import {logo} from '../../assets/index';
-import { HeartOutlined , SearchOutlined , ShoppingCartOutlined , UserOutlined } from '@ant-design/icons';
+import { logo } from '../../assets/index';
+import { HeartOutlined, SearchOutlined, ShoppingCartOutlined, UserOutlined, CloseOutlined } from '@ant-design/icons';
 const Header = () => {
-  return (
-    <div className='header'>
-        <div className='header__left'>
-            <img src={logo} alt='logo' />
+
+
+
+    const showHidenSearch = () => {
+        const searchHidden = document.querySelector('.header__search-hidden');
+        searchHidden.classList.toggle('show_search');
+    }
+    return (
+        <div className='header'>
+            <div className='header__left'>
+                <img src={logo} alt='logo' />
+            </div>
+            <div className='header__middle'>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Home
+                        </a>
+
+                    </li>
+                    <li>
+                        <a href="#">
+                            Shop
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            Pages
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className='header__left'>
+                <div className="header__left__search">
+                    <SearchOutlined
+                        className='header__icons search-icon'
+                        onClick={showHidenSearch}
+                    />
+                </div>
+                <div className="header__left__authentication">
+                    <UserOutlined className='header__icons' />
+                </div>
+                <div className="header__left__favorits">
+                    <HeartOutlined className='header__icons' />
+                </div>
+                <div className="header__left__cart">
+                    <ShoppingCartOutlined className='header__icons' />
+                </div>
+            </div>
+            <div 
+                className="header__search-hidden"
+                onAuxClick={showHidenSearch}
+            >
+                <CloseOutlined onClick={showHidenSearch}/>
+            </div>
         </div>
-        <div className='header__middle'>
-            <ul>
-                <li>Home</li>
-                <li>Products</li>
-                <li>Blog</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-            </ul>
-        </div>
-        <div className='header__left'>
-            <div className="header__left__search">
-                <SearchOutlined/>
-            </div>
-            <div className="header__left__authentication">
-                <UserOutlined/>
-            </div>
-            <div className="header__left__favorits">
-                <HeartOutlined/>
-            </div>
-            <div className="header__left__cart">
-                <ShoppingCartOutlined/>
-            </div>
-        </div>  
-    </div>
-  )
+    )
 }
 
 export default Header
