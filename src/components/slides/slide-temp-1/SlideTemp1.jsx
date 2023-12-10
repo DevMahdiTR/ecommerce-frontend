de5dfa508830f71f0e7a82a4456d9046e3222e8d
Mohamed El Mahdi Trabelsi <trabelsimahdi79@gmail.com>
2023-12-10 22:33:04 +0100

    Carouselle

--- src/components/slides/slide-temp-1/SlideTemp1.jsx
+++ src/components/slides/slide-temp-1/SlideTemp1.jsx
@@ -2,11 +2,11 @@ import './slidetemp1.scss';
 import {img_slider1} from '../../../assets/index';
 const SlideTemp1 = () => {
   return (
-    <div className='slide-temp-1'>
-        <div className="slide-temp-1__img">
+    <div className='slide-temp'>
+        <div className="slide-temp__img">
             <img src={img_slider1} alt="" />
         </div>
-        <div className="slide-temp-1__content">
+        <div className="slide-temp__content template-1">
             <p className='first'>SUMMBER 2023</p>
             <p className='second'>New Arriavla Collection</p>
             <a href="" className='slide-btn'>
